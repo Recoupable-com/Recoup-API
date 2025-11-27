6fc67df87dd2183201c4b596f43a87593266d0a5
Sweets Sweetman <sweetmantech@gmail.com>
2025-11-27 14:28:35 -0500

    remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
   env: {
     RESOURCE_WALLET_ADDRESS: process.env.RESOURCE_WALLET_ADDRESS,
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
