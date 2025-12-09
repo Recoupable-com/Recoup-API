547e7fc33d10cf49e5bb27207e5837a9565029fb
Sweets Sweetman <sweetmantech@gmail.com>
2025-12-09 11:37:18 -0500

    Fix incorrect JSDoc parameter documentation.

--- lib/supabase/account_api_keys/insertApiKey.ts
+++ lib/supabase/account_api_keys/insertApiKey.ts
@@ -4,8 +4,8 @@ import type { Database } from "@/types/database.types";
 /**
  * Inserts an API key into the database
  *
- * @param input - The input object containing the name, artist_address, and key_hash
- * @param input.name - The input object containing the name, artist_address, and key_hash
+ * @param input - The input object containing the name, account, and key_hash
+ * @param input.name - The input object containing the name, account, and key_hash
  * @param input.account - The account ID
  * @param input.key_hash - The hash of the API key
  * @returns The inserted API key
